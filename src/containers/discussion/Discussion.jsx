4879a63756622309cdc08d77c7a62b6d17bed5b1
Bulat Akhmetzyanov <r1ghtnowcp2@gmail.com>
2021-12-27 12:37:52 +0300

    second commit

--- src/containers/discussion/Discussion.jsx
+++ src/containers/discussion/Discussion.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Container, MainText } from '../../components'
 import { stand_girl } from '../../assets/images/images.js'
 
