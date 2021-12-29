977b153c720499add87329253961437d9b2f7a1d
Bulat Akhmetzyanov <r1ghtnowcp2@gmail.com>
2021-12-29 09:28:42 +0300

    html-тег и некоторые изменены

--- src/components/text/MainText.jsx
+++ src/components/text/MainText.jsx
@@ -8,17 +8,14 @@ const MainText = styled.p`
   line-height: ${props => (props.introText ? '1.75791em' : '1.457em')};
 
   @media (max-width: 1200px) {
-    font-size: ${props => (props.introText ? '20px' : '24px')};
+    font-size: ${props => (props.introText ? '20px' : '22px')};
   }
-
   @media (max-width: 768px) {
     font-size: 22px;
   }
-
   @media (max-width: 544px) {
     font-size: 20px;
   }
-
   @media (max-width: 320px) {
     font-size: 18px;
   }
