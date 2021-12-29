7acbe9d72e8dc0023859369713c5a7152490952b
Bulat Akhmetzyanov <r1ghtnowcp2@gmail.com>
2021-12-29 15:23:16 +0300

    Созданы основные стили

---
+++ src/components/title/MainTitle.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+const MainTitle = styled.h3`
+  font-family: Larsseit;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 1.4375em;
+  letter-spacing: 0.06em;
+  text-transform: uppercase;
+  color: #22356f;
+
+  @media (max-width: 544px) {
+    font-size: 14px;
+  }
+  @media (max-width: 320px) {
+    font-size: 12.5px;
+  }
+`
+
+export default MainTitle
