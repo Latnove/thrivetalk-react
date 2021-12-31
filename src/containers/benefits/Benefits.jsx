1f9f8bec599ab2038959ef4956df23c31d223683
Bulat Akhmetzyanov <r1ghtnowcp2@gmail.com>
2021-12-31 17:12:22 +0300

    Создан функциональный компонент

---
+++ src/containers/benefits/Benefits.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Block = styled.section`
+  width: 100%;
+  height: 100vh;
+`
+export default function Benefits() {
+  return <Block></Block>
+}
