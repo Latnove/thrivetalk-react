c5cc94f8c4a85b6a3ecade64548449ba62531074
Bulat Akhmetzyanov <r1ghtnowcp2@gmail.com>
2021-12-31 17:16:29 +0300

    Реализован scroll до Services-блока по клику на MainTitle

--- src/containers/services/Services.jsx
+++ src/containers/services/Services.jsx
@@ -1,24 +1,62 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import styled from 'styled-components'
-import MainTitle from '../../components/title/MainTitle.jsx'
+import { MainTitle } from '../../components/index.js'
 import { ServiceFirstPart, ServiceSecondPart } from './'
 
 const Inner = styled.div`
   padding: 70px 0 0 0;
+  @media (max-width: 768px) {
+    padding: 50px 0 0 0;
+  }
+  @media (max-width: 544px) {
+    padding: 40px 0 0 0;
+  }
+  @media (max-width: 320px) {
+    padding: 30px 0 0 0;
+  }
 `
-const Title = styled(MainTitle)`
-  opacity: 0.5;
+const Row = styled.div`
   text-align: center;
   margin: 0 auto;
+  & ${MainTitle} {
+    opacity: 0.5;
+    cursor: pointer;
+    display: inline;
+    :hover {
+      opacity: 0.7;
+      transition: opacity 0.2s;
+    }
+  }
 `
 
 export default function Services() {
+  const navRef = useRef(null)
+  const servicesRef = useRef(null)
+
+  useEffect(() => {
+    navRef.current = document.querySelector('header')
+  }, [])
+
+  function clickTitle() {
+    const scrollValue =
+      servicesRef.current.getBoundingClientRect().top -
+      navRef.current.offsetHeight
+
+    window.scrollBy({
+      top: scrollValue,
+      behavior: 'smooth',
+    })
+  }
+
   return (
-    <div id="services">
+    <section ref={servicesRef} id="services">
       <Inner>
-        <Title>WE CAN HELP YOU WITH</Title>
+        <Row>
+          <MainTitle onClick={clickTitle}>WE CAN HELP YOU WITH</MainTitle>
+        </Row>
         <ServiceFirstPart />
+        {/* <ServiceSecondPart /> */}
       </Inner>
-    </div>
+    </section>
   )
 }
