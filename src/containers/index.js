fe911a898cd6e56b8ae874044df52d12da240830
Bulat Akhmetzyanov <r1ghtnowcp2@gmail.com>
2021-12-31 17:13:14 +0300

    Export Benefits.jsx

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,6 +2,7 @@ export { default as Discussion } from './discussion/Discussion'
 export { default as Intro } from './intro/Intro'
 export { default as About } from './about/About'
 export { default as Services } from './services/Services'
+export { default as Benefits } from './benefits/Benefits'
 
 export { default as Navbar } from './layout/navbar/Navbar'
 export { default as Footer } from './layout/footer/Footer'
