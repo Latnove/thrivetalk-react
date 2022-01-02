f3fe35354c1649ff5f96ab474c289e32a72ee9ee
Bulat Akhmetzyanov <r1ghtnowcp2@gmail.com>
2022-01-03 00:29:38 +0300

    Настроена взаимосвязь между ServiceItem, адаптивность блоков

--- src/containers/services/service-first-part/ServiceFirstPart.jsx
+++ src/containers/services/service-first-part/ServiceFirstPart.jsx
@@ -1,36 +1,42 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import { ServiceItem } from '../../../components/'
 
 const Inner = styled.div`
-  margin-top: 24px;
+  margin: 24px 0 0 0;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-
   & > div {
+    width: 25%;
     max-height: 440px;
   }
   @media (max-width: 1200px) {
-    margin-top: 20px;
+    margin: 20px 0 0 0;
     & > div {
-      max-height: 320px;
+      max-height: 300px;
+    }
+  }
+  @media (max-width: 992px) {
+    & > div {
+      width: 50%;
     }
   }
   @media (max-width: 544px) {
-    margin-top: 16px;
+    margin: 16px 0 0 0;
     & > div {
-      max-height: 275px;
+      max-height: 230px;
     }
   }
   @media (max-width: 440px) {
-    margin-top: 18px;
+    margin: 18px 0 0 0;
     & > div {
+      width: 100%;
       max-height: 280px;
     }
   }
   @media (max-width: 320px) {
-    margin-top: 16px;
+    margin: 16px 0 0 0;
     & > div {
       max-height: 240px;
     }
@@ -38,48 +44,12 @@ const Inner = styled.div`
 `
 
 export default function ServiceFirstPart() {
-  const [valueWidth, setValueWidth] = useState(null)
-
-  useEffect(() => {
-    isSaveItemWidth()
-
-    window.addEventListener('resize', () => {
-      isSaveItemWidth()
-    })
-  }, [])
-
-  function isSaveItemWidth() {
-    if (window.matchMedia('(min-width: 993px)').matches) {
-      setValueWidth('25%')
-    } else if (window.matchMedia('(max-width: 440px)').matches) {
-      setValueWidth('100%')
-    } else if (window.matchMedia('(max-width: 992px)').matches) {
-      setValueWidth('50%')
-    }
-  }
-
   return (
     <Inner>
-      <ServiceItem
-        itemWidth={valueWidth}
-        itemColor="#FFE2DE"
-        titleText="Weight Lifting"
-      />
-      <ServiceItem
-        itemWidth={valueWidth}
-        itemColor="#C4E769"
-        titleText="Running & Spinning"
-      />
-      <ServiceItem
-        itemWidth={valueWidth}
-        itemColor="#62D0DF"
-        titleText="Pumping Iron"
-      />
-      <ServiceItem
-        itemWidth={valueWidth}
-        itemColor="#0052C1"
-        titleText="Swimming"
-      />
+      <ServiceItem itemColor="#FFE2DE" titleText="Weight Lifting" />
+      <ServiceItem itemColor="#C4E769" titleText="Running & Spinning" />
+      <ServiceItem itemColor="#62D0DF" titleText="Pumping Iron" />
+      <ServiceItem itemColor="#0052C1" titleText="Swimming" />
     </Inner>
   )
 }
---
+++ src/containers/services/service-second-part/ServiceSecondPart.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useRef } from 'react'
+import styled from 'styled-components'
+import { ServiceItem } from '../../../components/index.js'
+
+const Inner = styled.div`
+  margin: 120px 0 0 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  & > div {
+    width: 33.33%;
+    max-height: 585px;
+  }
+  @media (max-width: 1200px) {
+    margin: 90px 0 0 0;
+    & > div {
+      max-height: 320px;
+    }
+  }
+  @media (max-width: 992px) {
+    margin: 80px 0 0 0;
+    & > div {
+      width: 50%;
+      max-height: 390px;
+    }
+  }
+  @media (max-width: 768px) {
+    margin: 75px 0 0 0;
+  }
+  @media (max-width: 544px) {
+    margin: 60px 0 0 0;
+    & > div {
+      width: 80%;
+      max-height: 300px;
+    }
+    & > div:first-child,
+    & > div:first-child::before {
+      border-radius: 50px 50px 0 0;
+    }
+    & > div:last-child,
+    & > div:last-child::before {
+      border-radius: 0 0 40px 40px;
+    }
+  }
+  @media (max-width: 440px) {
+    margin: 60px 0 0 0;
+    & > div {
+      max-height: 330px;
+      width: 100%;
+    }
+    & > div:first-child,
+    & > div:first-child::before {
+      border-radius: 15% 15% 0 0;
+    }
+    & > div:last-child,
+    & > div:last-child::before {
+      border-radius: 0 0 10% 10%;
+    }
+  }
+  @media (max-width: 320px) {
+    margin: 45px 0 0 0;
+    & > div {
+      max-height: 320px;
+    }
+  }
+`
+
+export default function ServiceSecondPart() {
+  return (
+    <Inner>
+      <ServiceItem titleText="Physical Health" itemColor="#22356F" />
+      <ServiceItem titleText="Mental Health" itemColor="#0052C1" />
+      <ServiceItem titleText="Nutrition" itemColor="#76E58B" />
+      <ServiceItem titleText="Gymnastics" itemColor="#62D0DF" />
+      <ServiceItem titleText="Crossfit" itemColor="#DEE1FF" />
+      <ServiceItem titleText="Aerobics" itemColor="#8F00FF" />
+    </Inner>
+  )
+}
