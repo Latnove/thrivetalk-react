b5314f57b3e53afc41383d8c71c3262218124d79
Bulat Akhmetzyanov <r1ghtnowcp2@gmail.com>
2022-01-09 13:31:53 +0300

    export footer-images

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -3,6 +3,7 @@ import stand_girl from './discussion-images/f6d99ec32809b2909cd26d32dec3f462.jpg
 import cocktail_cup from './services-images/92a80edb8b973a793ef7422d5a3e4496.jpg'
 import sales_navigator from './contact-us-images/acfdbf3563c8350cda6977544fe04db8.jpg'
 import funny_man from './contact-us-images/eb608eb956037170f9f190249c60a153.jpg'
+import autumn_goodman from './footer-images/d424b642272b56a5cfa35e148bdd74b0.jpg'
 import test from './test-images/Kakie-proverki-mogut-zhdat-IP.jpg'
 
 export {
@@ -11,5 +12,6 @@ export {
   cocktail_cup,
   sales_navigator,
   funny_man,
+  autumn_goodman,
   test,
 }
