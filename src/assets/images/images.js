8b50f8b293b51ff486f31d7ed73167846a3e198b
Bulat Akhmetzyanov <r1ghtnowcp2@gmail.com>
2022-01-23 11:38:32 +0300

    Изменены расширения изображений на webp

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -1,10 +1,10 @@
-import interview from './intro-images/5f29d8a75b684bbc23a23ba5bc08f70c.jpg'
-import stand_girl from './discussion-images/f6d99ec32809b2909cd26d32dec3f462.jpg'
-import cocktail_cup from './services-images/92a80edb8b973a793ef7422d5a3e4496.jpg'
-import sales_navigator from './contact-us-images/acfdbf3563c8350cda6977544fe04db8.jpg'
-import funny_man from './contact-us-images/eb608eb956037170f9f190249c60a153.jpg'
-import autumn_goodman from './footer-images/d424b642272b56a5cfa35e148bdd74b0.jpg'
-import test from './test-images/Kakie-proverki-mogut-zhdat-IP.jpg'
+import interview from './intro-images/5f29d8a75b684bbc23a23ba5bc08f70c.webp'
+import stand_girl from './discussion-images/f6d99ec32809b2909cd26d32dec3f462.webp'
+import cocktail_cup from './services-images/92a80edb8b973a793ef7422d5a3e4496.webp'
+import sales_navigator from './contact-us-images/acfdbf3563c8350cda6977544fe04db8.webp'
+import funny_man from './contact-us-images/eb608eb956037170f9f190249c60a153.webp'
+import autumn_goodman from './footer-images/d424b642272b56a5cfa35e148bdd74b0.webp'
+import test from './test-images/Kakie-proverki-mogut-zhdat-IP.webp'
 
 export {
   interview,
